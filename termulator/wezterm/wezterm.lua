f1c2552decde22c1955c39eda22c22d27139288c
itsdend <marko.samarzija1@gmail.hr>
2025-06-16 03:28:50 +0200

    feat: wezterm new tabs and navigation

--- termulator/wezterm/wezterm.lua
+++ termulator/wezterm/wezterm.lua
@@ -1,9 +1,5 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
---hello
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 wezterm.on('update-right-status', function(window, pane)
 	window:set_right_status(window:active_workspace())
@@ -13,10 +9,7 @@ config.enable_wayland = true
 config.default_cursor_style = 'SteadyBlock'
 
 -- colors
-wezterm.log_info("Starting WezTerm with debugging enabled.")
-
 config.window_background_opacity = 0.9
--- This is where you actually apply your config choices
 config.colors = {
 	foreground = '#EB87B6',
 	background = '#24273A',
@@ -48,12 +41,6 @@ config.colors = {
 	}
 }
 
-function LOL()
-	local success, stdout, stderr = wezterm.run_child_process {'wl-paste'}
--- Remove the trailing newline (if it exists)
-  stdout = stdout:gsub("\n$", "")
-	return wezterm.action.SendString (stdout)
-end
 
 config.use_ime = true
 config.max_fps = 240
@@ -100,16 +87,6 @@ config.keys = {
 		action = wezterm.action.ActivatePaneDirection 'Left'
 	},
 	{
-		key = 'e',
-		mods = 'LEADER',
-		action = wezterm.action.ScrollByPage(-0.5)
-	},
-	{
-		key = 'r',
-		mods = 'LEADER',
-		action = wezterm.action.ScrollByPage(0.5)
-	},
-	{
 		key = 'g',
 		mods = 'LEADER',
 		action = wezterm.action.ScrollToBottom
@@ -140,16 +117,25 @@ config.keys = {
 		action = wezterm.action.ActivateCopyMode
 	},
 	{
-		key = 't',
+		key = 'r',
 		mods = 'LEADER',
 		action = wezterm.action.ReloadConfiguration
 	},
-	-- {
-	-- 	key = 't',
-	-- 	mods = 'SHIFT|ALT',
-	-- 	action = wezterm.action.SpawnTab 'DefaultDomain',
-	-- },
-	-- Switch to the default workspace
+	{
+		key = 'r',
+		mods = 'LEADER|SHIFT',
+		action = wezterm.action.PromptInputLine	{
+			description = 'Enter new name for tab',
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+					end
+				end),
+		},
+	},
 	{
 		key = 'u',
 		mods = 'LEADER',
@@ -191,7 +177,26 @@ config.keys = {
 
 		},
 	},
-	{ key = 'f', mods = 'LEADER', action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' },},
+	{
+		key = 't',
+		mods = 'LEADER',
+		action = wezterm.action.PromptInputLine	{
+			description = 'Enter new name for tab',
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(
+					wezterm.action.SpawnTab 'CurrentPaneDomain', pane
+					)
+					window:active_tab():set_title(line)
+					end
+				end),
+		},
+	},
+	{ key = 'f', mods = 'LEADER', action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES'},},
+	{ key = 'f', mods = 'LEADER|SHIFT', action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|TABS'},},
 	{ key = 'a', mods = 'LEADER', action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|DOMAINS' },},
 	{
 		key = 'e',
@@ -199,11 +204,6 @@ config.keys = {
 		action = wezterm.action.CharSelect {
 			copy_on_select = false
 		},
-	},
-	{
-		key="y", mods="LEADER", action= wezterm.action.Multiple{
-		wezterm.action.ReloadConfiguration,
-			LOL()}
 	},
 	{
       key = 'u',
@@ -217,19 +217,9 @@ config.keys = {
 config.font = wezterm.font('ComicShannsMono Nerd Font', { weight = 420}) -- 547 default
 config.font_size = 13
 config.cell_width = 1.1
--- config.font_size = 19.4
-
--- This doesn't work well on a dual screen setup, and is hopefully a temporary solution to the font rendering oddities
--- shown in https://github.com/wez/wezterm/issues/4096. Ideally I'll switch to using `dpi_by_screen` at some point,
--- but for now 11pt @ 109dpi seems to be the most stable for font rendering on my 38" ultrawide LG monitor here.
---
--- THIS IS THE CONFIG FOR XWAYLAND FOR THE FONT SIZE DEPENDING ON THE MONITOR
---
---
 wezterm.on('window-config-reloaded', function(window)
 	if wezterm.gui.screens().active.name == 'eDP-1' then
 		window:set_config_overrides({
-			-- font_size = 19.4 -- this is if its on xwayland
 			font_size = 12.9
 		})
 	else
@@ -238,8 +228,6 @@ wezterm.on('window-config-reloaded', function(window)
 		})
 	end
 end)
--- gpu
--- config.front_end = "OpenGL"
 
 for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
 	if gpu.backend == 'Vulkan' and gpu.device_type == 'IntegratedGpu' then
@@ -257,8 +245,4 @@ config.window_decorations = "NONE"
 
 config.window_close_confirmation = "NeverPrompt"
 
--- ubuntu
---config.default_domain = 'WSL:Ubuntu'
-
--- and finally, return the configuration to wezterm
 return config
